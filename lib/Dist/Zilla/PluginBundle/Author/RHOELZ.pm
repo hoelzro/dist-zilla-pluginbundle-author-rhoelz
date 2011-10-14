f7d173119f5ad00f3b6103c87fb6046c86e56885
Rob Hoelz <rob@hoelz.ro>
2011-10-14 08:19:38 -0500

    initial commit

---
+++ lib/Dist/Zilla/PluginBundle/Author/RHOELZ.pm
@@ -0,0 +1,22 @@
+## no critic (RequireUseStrict)
+package Dist::Zilla::PluginBundle::Author::RHOELZ;
+
+## use critic (RequireUseStrict)
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+# ABSTRACT:  A short description of Dist::Zilla::PluginBundle::Author::RHOELZ
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=head1 SEE ALSO
+
+=cut
