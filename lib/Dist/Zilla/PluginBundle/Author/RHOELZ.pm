d45844ec23bafde6514759f8c268f8beaac0b42c
Rob Hoelz <rob@hoelz.ro>
2011-10-15 20:01:15 -0500

    Add plugins and stuff

--- lib/Dist/Zilla/PluginBundle/Author/RHOELZ.pm
+++ lib/Dist/Zilla/PluginBundle/Author/RHOELZ.pm
@@ -5,18 +5,157 @@ package Dist::Zilla::PluginBundle::Author::RHOELZ;
 use strict;
 use warnings;
 
+use Moose;
+
+with 'Dist::Zilla::Role::PluginBundle::Easy';
+
+sub configure {
+    my ( $self ) = @_;
+
+    $self->add_plugins([
+        GithubMeta => {
+            issues => 1,
+        },
+    ]);
+
+    $self->add_plugins([
+        'Git::Check' => {
+            allow_dirty => ['dist.ini', 'README.pod'],
+        },
+    ]);
+
+    $self->add_plugins([
+        NextRelease => {
+            format => '%v %{MMMM dd yyyy}d',
+        },
+    ]);
+
+    $self->add_plugins([
+        'Git::Commit' => {
+            allow_dirty => [
+                'dist.ini',
+                'README.pod',
+                'Changes',
+            ],
+        },
+    ]);
+
+    $self->add_plugins([
+        'Git::Tag' => {
+            tag_format  => '%v',
+            tag_message => '%v',
+        },
+    ]);
+
+    $self->add_plugins([
+        'Git::NextVersion' => {
+            first_version  => '0.01',
+            version_regexp => '^(\d+\.\d+)$',
+        },
+    ]);
+
+    $self->add_plugins([
+        ReadmeAnyFromPod => {
+            type     => 'pod',
+            filename => 'README.pod',
+            location => 'root',
+        },
+    ]);
+
+    $self->add_plugins(
+        'GatherDir',
+        'PruneCruft',
+        'MetaYAML',
+        'License',
+        'Readme',
+        'ModuleBuild',
+        'Manifest',
+        'PodCoverageTests',
+        'PodSyntaxTests',
+        'Test::DistManifest',
+        'Test::Kwalitee',
+        'Test::Compile',
+        'Test::Perl::Critic',
+        'TestRelease',
+    );
+
+    $self->add_plugins([
+        PruneFiles => {
+            filename => ['dist.ini', 'weaver.ini'],
+        },
+    ]);
+
+    $self->add_plugins(
+        'ConfirmRelease',
+        'UploadToCPAN',
+        'PkgVersion',
+        'PodWeaver',
+    );
+}
+
+__PACKAGE__->meta->make_immutable;
+
 1;
 
 __END__
 
-# ABSTRACT:  A short description of Dist::Zilla::PluginBundle::Author::RHOELZ
+# ABSTRACT:  BeLike::RHOELZ when you build your distributions.
 
 =head1 SYNOPSIS
 
+  ; in your dist.ini
+  [@Author::RHOELZ]
+
 =head1 DESCRIPTION
 
-=head1 FUNCTIONS
+This is the plugin bundle that RHOELZ uses to build distributions.  It is
+equivalent to the following:
+
+  [GithubMeta]
+  issues = 1
+  [Git::Check]
+  allow_dirty = dist.ini
+  allow_dirty = README.pod
+  [NextRelease]
+  format = %v %{MMMM dd yyyy}d
+  [Git::Commit]
+  allow_dirty = dist.ini
+  allow_dirty = README.pod
+  allow_dirty = Changes
+  [Git::Tag]
+  tag_format  = %v
+  tag_message = %v
+  [Git::NextVersion]
+  first_version  = 0.01
+  version_regexp = ^(\d+\.\d+)$
+  [ReadmeAnyFromPod]
+  type     = pod
+  filename = README.pod
+  location = root
+  [GatherDir]
+  [PruneCruft]
+  [MetaYAML]
+  [License]
+  [Readme]
+  [ModuleBuild]
+  [Manifest]
+  [PodCoverageTests]
+  [PodSyntaxTests]
+  [Test::DistManifest]
+  [Test::Kwalitee]
+  [Test::Compile]
+  [Test::Perl::Critic]
+  [TestRelease]
+  [PruneFiles]
+  filename = dist.ini
+  filename = weaver.ini
+  [ConfirmRelease]
+  [UploadToCPAN]
+  [PkgVersion]
+  [PodWeaver]
 
 =head1 SEE ALSO
+
+L<Dist::Zilla>
 
 =cut
